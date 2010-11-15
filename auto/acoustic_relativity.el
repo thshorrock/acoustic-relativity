d2a34fa88f99f3b2d464d90ed07935c9a3d411dd
Tom Shorrock <t.h.shorrock@gmail.com>
2010-11-15 19:31:34 +0000

    initialised repo

---
+++ auto/acoustic_relativity.el
@@ -0,0 +1,172 @@
+(TeX-add-style-hook "acoustic_relativity"
+ (lambda ()
+    (LaTeX-add-bibliographies
+     "../../tshorrock_thesis/bib/tshorrock")
+    (LaTeX-add-labels
+     "eqn:#2"
+     "sec:introduction"
+     "sec:measurement"
+     "eqn:radar"
+     "eqn:radarDistance"
+     "sec:Maxwell"
+     "sec:int:EM"
+     "eqn:Rcontinuity"
+     "eqn:REuler"
+     "eqn:NRcontinuity"
+     "eqn:NREuler"
+     "eqn:WavePhi"
+     "eqn:WavevA"
+     "eqn:WavephiREG"
+     "eqn:WavevvREG"
+     "eqn:WavePhiMax"
+     "eqn:WavevAMax"
+     "eqn:M1"
+     "eqn:M2"
+     "eqn:M3"
+     "eqn:M4"
+     "eqn:rho"
+     "eqn:vJ"
+     "sec:Interpretations"
+     "sec:NonlinearProp"
+     "sec:transversivity"
+     "sec:int:spin"
+     "eqn:Lagrangian"
+     "eqn:EL"
+     "eqn:LFL"
+     "eqn:Helicity"
+     "footnote:dimensionallity_footnote"
+     "sec:bubble"
+     "sec:KMoriginal"
+     "eqn:WaveEqn"
+     "eqn:Ansatz"
+     "eqn:pressureBC"
+     "eqn:VelocityPotNR"
+     "eqn:BernoulliNR"
+     "sec:measurement:alterations"
+     "eqn:RelVPot"
+     "eqn:VelocityPot"
+     "eqn:Bernoulli"
+     "eqn:phiDefn"
+     "eqn:dtAnsatz"
+     "eqn:drAnsatz"
+     "eqn:Combo1"
+     "eqn:Combo2"
+     "eqn:Combo3"
+     "eqn:Tau"
+     "eqn:IncidentWave"
+     "eqn:RKM"
+     "eqn:KM"
+     "eqn:soundspeed"
+     "eqn:pshort"
+     "eqn:nrootp"
+     "sec:surface_pressure"
+     "sec:viscosity"
+     "eqn:RelStress"
+     "eqn:RelStressTrace"
+     "eqn:sigmarr1"
+     "eqn:sigmarr2"
+     "eqn:RelStressTrace2"
+     "eqn:sigmarr3"
+     "eqn:NRsigmarr"
+     "eqn:sigmarr4"
+     "fig:R1rad"
+     "fig:R1vel"
+     "fig:R3rad"
+     "fig:R3vel"
+     "fig:R5rad"
+     "fig:R5vel"
+     "fig:BubbleResponse"
+     "sec:measurement:scatteringxs"
+     "eqn:sigmaff"
+     "eqn:pff"
+     "fig:nxs_low01"
+     "fig:nxs_low1"
+     "fig:nxs_high3"
+     "fig:nxs_high5"
+     "fig:nxs_high"
+     "sec:discussion")
+    (TeX-add-symbols
+     '("nlistcompact" 1)
+     '("nlist" 1)
+     '("vect" 1)
+     '("bivector" 1)
+     '("multi" 2)
+     '("pseudo" 1)
+     '("tri" 1)
+     '("bi" 1)
+     '("mono" 1)
+     '("scalar" 1)
+     '("adjoint" 1)
+     '("Chapref" 1)
+     '("chapref" 1)
+     '("scope" 1)
+     '("given" 2)
+     '("abs" 1)
+     '("lrsquare" 1)
+     '("lr" 1)
+     '("Figref" 1)
+     '("figref" 1)
+     '("Eqnref" 1)
+     '("eqnref" 1)
+     '("Secref" 1)
+     '("secref" 1)
+     '("eqal" 2)
+     '("eqa" 1)
+     '("subl" 2)
+     '("sub" 1)
+     "epsillon"
+     "dprime"
+     "del"
+     "vdel"
+     "tm"
+     "tmp"
+     "tp"
+     "tpp"
+     "dr"
+     "dt"
+     "dtau"
+     "Tr"
+     "half"
+     "aether"
+     "aetherial"
+     "Poincare"
+     "Tgi"
+     "OmegaEM"
+     "vf"
+     "J"
+     "vJ"
+     "vE"
+     "vB"
+     "vEnr"
+     "vBnr"
+     "M"
+     "vP"
+     "Dt"
+     "g"
+     "vu"
+     "vA"
+     "vv")
+    (TeX-run-style-hooks
+     "amsthm"
+     "amsmath"
+     "subfig"
+     "SIunits"
+     "mediumspace"
+     "textstyle"
+     "amssymb"
+     "graphicx"
+     "natbib"
+     "sort&compress"
+     "square"
+     "super"
+     "ifpdf"
+     "emp"
+     "latex2e"
+     "art10"
+     "article"
+     "10pt"
+     "fleqn"
+     "draft"
+     "showtrims"
+     "oldfontcommands")))
+
